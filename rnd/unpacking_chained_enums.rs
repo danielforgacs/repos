0c56f88b7121530cc01a5bf940cb0abc17dadb26
df@ <@>
2021-08-30 11:16:00 +0100

    Added rnd unpacking chained enums.

---
+++ rnd/unpacking_chained_enums.rs
@@ -0,0 +1,30 @@
+#[derive(Debug)]
+enum C {
+    Cc(String),
+}
+
+#[derive(Debug)]
+enum B {
+    Bb(C),
+}
+
+#[derive(Debug)]
+enum A {
+    Aa(B),
+}
+
+fn main() {
+    let _result: String = match get() {
+        A::Aa(res) => match res {
+            B::Bb(res2) => match res2 {
+                C::Cc(res3) => res3
+            }
+        }
+    };
+    dbg!(&_result);
+    // println!("{:#?}", &_result);
+}
+
+fn get() -> A {
+    A::Aa(B::Bb(C::Cc(String::from("OK"))))
+}
