43fda41b0afe92f6257dbb0ce9197018c36b91c0
df@ <@>
2021-08-30 02:52:45 +0100

    OK

--- repos/src/main.rs
+++ repos/src/main.rs
@@ -3,11 +3,12 @@ fn main() {
         Ok(pwd) => pwd,
         _ => std::path::PathBuf::new(),
     };
+    
     let rootname: &str = match pwd.as_path().to_str() {
         Some(pwd3) => pwd3,
         None => "",
     };
-
+    // println!("..rootname: {}", rootname);
     
     let root = match std::fs::read_dir(rootname) {
         Ok(dir) => dir,
@@ -17,7 +18,10 @@ fn main() {
         }
     };
 
+    // dbg!(&root);
+
     for dir_opt in root {
+        // dbg!(&dir_opt);
         let dir = match dir_opt {
             Ok(dir) => dir,
             _ => continue,
@@ -28,14 +32,16 @@ fn main() {
             _ => continue,
         };
 
-        let githead: String = format!("{}{}/.git/HEAD", rootname, stringdir);
+        let githead: String = format!("{}/{}/.git/HEAD", rootname, stringdir);
+        // dbg!(&githead);
         let githead = match std::fs::read_to_string(&githead) {
             Ok(head) => head.trim().to_string(),
             _ => continue,
         };
+        // dbg!(&githead);
 
-        if githead != "ref: refs/heads/master\n" {
-            println!("[DIR]: {: <20} [HEAD]: {}", stringdir, githead);
+        if githead != "ref: refs/heads/master" {
+            println!("{: <35} {}", stringdir, githead);
         };
     };
 }
