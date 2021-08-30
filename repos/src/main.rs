f3a1f0ddb9ff2fda0333b2a2a498d3c5e142533f
df@ <@>
2021-08-30 03:00:06 +0100

    Named expected types expliitly.

--- repos/src/main.rs
+++ repos/src/main.rs
@@ -18,24 +18,21 @@ fn main() {
     };
 
     for dir_opt in root {
-        // dbg!(&dir_opt);
-        let dir = match dir_opt {
+        let dir: std::fs::DirEntry = match dir_opt {
             Ok(dir) => dir,
             _ => continue,
         };
 
-        let stringdir = match dir.file_name().into_string() {
+        let stringdir: String = match dir.file_name().into_string() {
             Ok(dirn) => dirn,
             _ => continue,
         };
 
         let githead: String = format!("{}/{}/.git/HEAD", rootname, stringdir);
-        // dbg!(&githead);
-        let githead = match std::fs::read_to_string(&githead) {
+        let githead: String = match std::fs::read_to_string(&githead) {
             Ok(head) => head.trim().to_string(),
             _ => continue,
         };
-        // dbg!(&githead);
 
         if githead != "ref: refs/heads/master" {
             println!("{: <35} {}", stringdir, githead);
