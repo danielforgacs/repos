d7760642703834da4c61953a4b7c8ac702b437fa
df@ <@>
2021-12-01 02:00:01 +0000

    wip status.

--- src/main.rs
+++ src/main.rs
@@ -59,6 +59,21 @@ impl Repo {
         git_output.sort_by_key(|a| a.to_lowercase());
         self.branches = git_output;
     }
+
+    fn update_status(&mut self) {
+        let output = std::process::Command::new("git")
+            .arg("status")
+            .arg("--porcelain")
+            .current_dir(&self.path)
+            .output()
+            .expect("can't get status.");
+        for line in String::from_utf8(output.stdout)
+            .expect("can't get status output")
+            .lines() {
+                println!("{:?}", line)
+
+            }
+    }
 }
 
 impl Tui {
@@ -154,7 +169,10 @@ fn main() {
         .iter()
         .map(|path| Repo::new(path.to_path_buf(), "status"))
         .collect();
-    tui(repos);
+    // tui(repos);
+    for mut r in repos {
+        r.update_status();
+    }
 }
 
 fn get_dev_dir() -> PathBuf {
