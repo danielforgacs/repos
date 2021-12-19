1e54fe40e2f8f35054a281dd290b7da436b9ca0c
df@ <@>
2021-12-19 01:40:52 +0000

    mkDummyRepos.sh

--- mkDummyRepos.sh
+++ mkDummyRepos.sh
@@ -9,7 +9,6 @@ cd $DEVDIR && repodir="__DEL_WIP_REPO_01" && mkdir $repodir && cd $repodir
 git init && git commit --allow-empty -m "init."
 cd $startdir
 # ///////////////////////
-# ///////////////////////
 cd $DEVDIR && repodir="__DEL_WIP_REPO_02" && mkdir $repodir && cd $repodir
 git init && git commit --allow-empty -m "init."
 git checkout -b dev && git checkout -b hotfix && git checkout -b rnd && git checkout -b ticket
@@ -22,6 +21,14 @@ cd $startdir
 # ///////////////////////
 cd $DEVDIR && repodir="__DEL_WIP_REPO_04" && mkdir $repodir && cd $repodir
 git init && git commit --allow-empty -m "init."
+touch untracked_file
+git add untracked_file && git commit -m 'commit 1'
+echo content > untracked_file
+cd $startdir
+# ///////////////////////
+cd $DEVDIR && repodir="__DEL_WIP_REPO_05" && mkdir $repodir && cd $repodir
+git init && git commit --allow-empty -m "init."
+git checkout -b dev && git checkout -b hotfix && git checkout -b rnd && git checkout -b ticket
 touch untracked_file
 git add untracked_file && git commit -m 'commit 1'
 echo content > untracked_file
