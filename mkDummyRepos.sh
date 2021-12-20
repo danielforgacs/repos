d38ba1a1c088cb488bb898981b8484b468375b5d
df@ <@>
2021-12-20 00:59:24 +0000

    added test repo with too many branches.

--- mkDummyRepos.sh
+++ mkDummyRepos.sh
@@ -33,3 +33,8 @@ touch untracked_file
 git add untracked_file && git commit -m 'commit 1'
 echo content > untracked_file
 cd $startdir
+# ///////////////////////
+cd $DEVDIR && repodir="__DEL_WIP_REPO_06" && mkdir $repodir && cd $repodir
+git init && git commit --allow-empty -m "init."
+git branch branch-1 && git branch branch-2 && git branch branch-3 && git branch branch-4 && git branch branch-5 && git branch branch-6 && git branch branch-7 && git branch branch-8 && git branch branch-9 && git branch branch-10 && git branch branch-11 && git branch branch-12 && git branch branch-13 && git branch branch-14
+cd $startdir
