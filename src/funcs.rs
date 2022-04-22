bfc5998d89845ad971c9ebde6b7ae50d03bcbdf4
df@ <@>
2022-04-22 23:58:50 +0100

    getting root path from arg or env var; custom error

--- src/funcs.rs
+++ src/funcs.rs
@@ -1,9 +1,23 @@
 use crate::prelude::*;
 
-pub fn get_root_path() {
+pub fn get_root_path() -> ReposError<PathBuf> {
     let matches = Command::new("repos")
         .arg(
             Arg::new("rootpath")
         )
         .get_matches();
+    let mut buff = PathBuf::new();
+    let mut path_arg = Path::new("");
+    let mut devdir = String::new();
+    if let Some(rootdir) = matches.value_of("rootpath") {
+        buff.push(rootdir);
+        path_arg = Path::new(rootdir);
+    } else {
+        devdir = var(DEV_DIR_ENV_VAR)?;
+        path_arg = Path::new(&devdir);
+    }
+    if !path_arg.is_dir() {
+        return Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, "oh no!")));
+    }
+    Ok(path_arg.to_path_buf())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
 mod funcs;
 
 mod prelude {
+    pub use std::path::{PathBuf, Path};
+    pub use std::env::var;
     pub use clap::{Arg, Command};
-
     pub use crate::funcs::get_root_path;
+
+    pub type ReposError<T> = Result<T, Box<dyn std::error::Error>>;
+    pub const DEV_DIR_ENV_VAR: &str = "DEVDIR";
 }
 
 use prelude::*;
 
 fn main() {
-    let root_path = get_root_path();
+    let root_path = match get_root_path() {
+        Err(err) => {
+            println!("{}", err);
+            return;
+        }
+        Ok(path) => path,
+    };
     dbg!(root_path);
 }
