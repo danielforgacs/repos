c0ef8a7587b2c56d1075de3ef6bfb355c66eeced
df@ <@>
2022-04-24 13:40:17 +0100

    refactored test repos makes

--- mkTestRepos.sh
+++ mkTestRepos.sh
@@ -4,29 +4,24 @@ export DEVDIR_ORIGINAL=$DEVDIR
 export DEVDIR=$td
 echo $td
 
-mkdir $td/no_commit && \
-    cd $td/no_commit && \
+mkdir $td/no_commit && cd $td/no_commit && \
     git init
 
-mkdir $td/just_init && \
-    cd $td/just_init && \
+mkdir $td/just_init && cd $td/just_init && \
     git init && \
     git commit --allow-empty -m 'Init.'
 
-mkdir $td/new_file && \
-    cd $td/new_file && \
+mkdir $td/new_file && cd $td/new_file && \
     git init && \
     touch untracked_new_file
 
-mkdir $td/new_file_staged && \
-    cd $td/new_file_staged && \
+mkdir $td/new_file_staged && cd $td/new_file_staged && \
     git init && \
     git commit --allow-empty -m 'Init.' && \
     echo "nothing." > staged_new_file && \
     git add staged_new_file
 
-mkdir $td/modified_files && \
-    cd $td/modified_files && \
+mkdir $td/modified_files && cd $td/modified_files && \
     git init && \
     git commit --allow-empty -m 'Init.' && \
     echo "nothing." > some_file && \
