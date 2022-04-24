8c7deed7aec1df58d7756b5916606439a9c5b5fd
df@ <@>
2022-04-24 16:20:39 +0100

    getting current branch

--- src/repo.rs
+++ src/repo.rs
@@ -24,6 +24,24 @@ impl Repo {
     }
 
     pub fn get_current_branch(&self) -> String {
-        String::from("n/a")
+        use git2::{ErrorCode};
+
+        #[derive(Eq, PartialEq)]
+        enum Format {
+            Long,
+            Short,
+            Porcelain,
+        }
+
+
+        let head = match self.repo.head() {
+            Ok(head) => Some(head),
+            Err(ref e) if e.code() == ErrorCode::UnbornBranch || e.code() == ErrorCode::NotFound => {
+                None
+            }
+            Err(e) => return String::from("n/a"),
+        };
+        let head = head.as_ref().and_then(|h| h.shorthand());
+        head.unwrap_or("HEAD (no branch)").to_string()
     }
 }
