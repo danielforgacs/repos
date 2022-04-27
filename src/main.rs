83068889c5c748d3a533604b79c25591538b840f
df@ <@>
2022-04-28 00:20:32 +0100

    added crossterm event loop with non blocking input key input events - I used the "event-poll-read.rs" example

--- src/main.rs
+++ src/main.rs
@@ -23,23 +23,54 @@ mod prelude {
 
 use prelude::*;
 
+use std::{time::Duration};
+use crossterm::{
+    cursor::position,
+    event::{poll, read, Event, KeyCode},
+    terminal::{disable_raw_mode, enable_raw_mode},
+    Result,
+};
+
+const UPDATE_DELAY_SECS: f32 = 1.5;
+
+
+fn print_events(root_path: &PathBuf) -> ReposError<()> {
+    loop {
+        if poll(Duration::from_secs_f32(UPDATE_DELAY_SECS))? {
+            let event = read()?;
+            println!("Event::{:?}\r", event);
+            if event == Event::Key(KeyCode::Char('c').into()) {
+                println!("Cursor position: {:?}\r", position());
+            }
+            if event == Event::Key(KeyCode::Esc.into()) {
+                break;
+            }
+        } else {
+            for repo_path in find_git_repos_in_dir(&root_path)? {
+                let repo = Repo::new(&repo_path)?;
+                println!("{}::{}::{}::{}\r",
+                    repo.get_name(),
+                    repo.get_current_branch(),
+                    repo.get_status(),
+                    repo.get_branches().join(" ")
+                )
+            };
+        }
+    }
+    Ok(())
+}
+
 fn main() -> ReposError<()> {
     let root_path = match get_root_path() {
         Err(err) => {
-            println!("{}", err);
+            println!("{}\r", err);
             return Ok(());
         }
         Ok(path) => path,
     };
-    println!("--> {}", root_path.to_string_lossy());
-    for repo_path in find_git_repos_in_dir(&root_path)? {
-        let repo = Repo::new(&repo_path)?;
-        println!(r#"{}::{}::{}::{}"#,
-            repo.get_name(),
-            repo.get_current_branch(),
-            repo.get_status(),
-            repo.get_branches().join(" ")
-        )
-    };
+    println!("--> {}\r", root_path.to_string_lossy());
+    enable_raw_mode()?;
+    print_events(&root_path)?;
+    disable_raw_mode();
     Ok(())
 }
